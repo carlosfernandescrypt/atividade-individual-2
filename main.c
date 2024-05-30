712686329f7fb1f60b40e9fd7cc905543c039d99
Carlos Fernandes <69088161+carlosfernandescrypt@users.noreply.github.com>
2024-05-30 19:33:11 -0300

    funnções criarNo e InserirNo

--- main.c
+++ main.c
@@ -9,3 +9,26 @@ typedef struct No {
     struct No *anterior;
     struct No *proximo;
 } No;
+
+// Função para criar um novo nó
+No* criarNo(char *artista, char *musica) {
+    No *novoNo = (No*)malloc(sizeof(No));
+    strcpy(novoNo->artista, artista);
+    strcpy(novoNo->musica, musica);
+    novoNo->anterior = novoNo->proximo = novoNo;
+    return novoNo;
+}
+
+// Função para inserir um nó na lista
+void inserirNo(No **head, char *artista, char *musica) {
+    No *novoNo = criarNo(artista, musica);
+    if (*head == NULL) {
+        *head = novoNo;
+    } else {
+        No *ultimo = (*head)->anterior;
+        novoNo->proximo = *head;
+        (*head)->anterior = novoNo;
+        novoNo->anterior = ultimo;
+        ultimo->proximo = novoNo;
+    }
+}
