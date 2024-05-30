528aad7c58316f69f2c71aeb34cc69a1bc0b1993
Carlos Fernandes <69088161+carlosfernandescrypt@users.noreply.github.com>
2024-05-30 19:31:02 -0300

    adicionado main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Estrutura para representar um nó da lista circular duplamente encadeada
+typedef struct No {
+    char artista[100];
+    char musica[100];
+    struct No *anterior;
+    struct No *proximo;
+} No;
+
+// Declarações das funções utilizadas no sistema de playlist
+No* criarNo(char *artista, char *musica);
+void inserirNo(No **cabeca, char *artista, char *musica);
+void lerArquivoEInserirNaLista(No **cabeca);
+void exibirPlaylist(No *cabeca);
+void exibirPlaylistOrdenada(No *cabeca);
+void inserirMusicaUsuario(No **cabeca);
+void removerMusica(No **cabeca, char *musicaParaRemover);
+void removerMusicaUsuario(No **cabeca);
+void buscarMusica(No *cabeca, char *musicaParaBuscar);
+void buscarMusicaUsuario(No *cabeca);
+void avancarMusica(No **musicaAtual);
+void retornarMusica(No **musicaAtual);
+
+#endif // MAIN_H
